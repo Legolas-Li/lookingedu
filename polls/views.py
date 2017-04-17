6da5242719e2bd50c75b9e638bf61dd85e7d2eb0
legolas <legolasli@qiyi.com>
2017-04-18 01:50:42 +0800

    Add static file & templates Set index.html
    Signed-off-by: legolas <legolasli@qiyi.com>

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
 
+
+def index(request):
+    print 111111111
+    return render(request, "index.html")
